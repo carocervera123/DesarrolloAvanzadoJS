52ad484cd58813c484763dec6d76c1ce069915e1
Carolina Cervera Torres <caro.cer.tor123@gmail.com>
2025-06-11 23:09:50 -0600

    mi-exploracion-espacial

---
+++ 4.1.1-Node.js/proyecto-7/planetas.js
@@ -0,0 +1,25 @@
+// planetas.js
+const planetas = [
+    {
+        nombre: "Titán",
+        descripcion: "La luna más grande de Saturno, con lagos de metano.",
+        descubiertoEn: "1655"
+    },
+    {
+        nombre: "Próxima Centauri b",
+        descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+        descubiertoEn: "2016"
+    },
+    {
+        nombre: "Kepler-186f",
+        descripcion: "El primer exoplaneta del tamaño de la Tierra encontrado en la zona habitable de otra estrella.",
+        descubiertoEn: "2014"
+    },
+    {
+        nombre: "HD 209458 b",
+        descripcion: "Un gigante gaseoso que se evapora, conocido como 'Osiris'.",
+        descubiertoEn: "1999"
+    }
+];
+
+module.exports = planetas;
